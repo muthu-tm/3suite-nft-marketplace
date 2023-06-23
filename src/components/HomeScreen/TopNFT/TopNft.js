3fda5aca860ca33806c0d6e1e6bb1aabf103ba4e
vaishu-shan <vaishnavishanmugam@usquare.info>
2023-06-23 20:58:30 +0530

    Home page UI design completed.

--- src/components/HomeScreen/TopNFT/TopNft.js
+++ src/components/HomeScreen/TopNFT/TopNft.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./topNft.css"
 import Heading from '../../../container/Heading/Heading';
+import NftCard from '../../../container/NFTCard/NftCard';
 
 function TopNft(props) {
     return (
         <div className='t-nft'>
             <Heading title="Top NFTs" />
+            <div className='nft-colln'>
+              <NftCard/>
+            </div>
             <div className="view-btn">
         <button className="view-more">
           View More <span style={{marginLeft:5}}>  &rarr; </span>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../components/HomeScreen/Banner/Banner';
 import TopCreator from '../components/HomeScreen/TopCreators/TopCreator';
 import TopNft from '../components/HomeScreen/TopNFT/TopNft';
+import CreateAndSell from '../components/HomeScreen/CreateSell/CreateAndSell';
 
 function HomePage(props) {
     return (
@@ -9,6 +10,7 @@ function HomePage(props) {
             <Banner/>
             <TopCreator/>
             <TopNft/>
+            <CreateAndSell/>
         </div>
     );
 }
