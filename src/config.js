c811c199a1cde584476019416b8b759eefc87147
vaishu-shann <ilukiabi@gmail.com>
2023-09-19 20:37:54 +0530

    Config error fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 // Sepoliaa
 const config = {
-  baseURL: "http://localhost:3001/v1/",
+  baseURL: "http://3.135.21.203:3001/v1/",
   defaultNetwork: 11155111,
   networkname: "Sepolia",
   etherscan: "https://sepolia.etherscan.io/tx/",
